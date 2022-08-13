cd41bc7ee10dc355a691a716b08321c424d430a3
bob <chenbob@hotmail.com>
2022-08-13 12:58:22 +0800

    msc init

---
+++ Msc/Msc.h
@@ -0,0 +1,32 @@
+﻿
+// Msc.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMscApp:
+// 有关此类的实现，请参阅 Msc.cpp
+//
+
+class CMscApp : public CWinApp
+{
+public:
+	CMscApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMscApp theApp;
